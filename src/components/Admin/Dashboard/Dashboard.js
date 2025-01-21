fca5f9b21724963c20d89a2c028aff00c4758b29
tausif40 <tausif6040@gmail.com>
2025-01-21 14:40:14 +0530

    update dashboard

--- src/components/Admin/Dashboard/Dashboard.js
+++ src/components/Admin/Dashboard/Dashboard.js
@@ -7,33 +7,36 @@ function Dashboard() {
 		<>
 			<AdminNav />
 
-			<div className='container py-4'>
+			<div className='h-[87vh] w-full bg-no-repeat ' style={{ backgroundImage: "url('/assets/img/homeCrouse/bed1.jpeg')" }}>
 
-				<div className=''>
-					<p className='text-3xl text-heading font-semibold '>Welcome to admin Panel</p>
-				</div>
+				<div className='container py-4'>
 
-				<div className='flex gap-8 mt-12 justify-end items-end flex-col'>
-					<div className='border px-4 pt-2 pb-4 rounded w-72'>
-						<p className='text-xl font-lg font-bold text-gray-700'>Image Category</p>
-						<p className='text-text mt-3 mb-4'>Create a new category for images</p>
-						<Link to='/admin/add-category'>
-							<button className='bg-gray-800 px-4 py-2 rounded-md text-white'>
-								Add Category
-							</button>
-						</Link>
+					<div className=''>
+						<p className='text-3xl text-heading font-semibold '>Welcome to admin Panel</p>
 					</div>
-					<div className='border px-4 pt-2 pb-4 rounded w-72'>
-						<p className='text-xl font-lg font-bold text-gray-700'>Upload Images</p>
-						<p className='text-text mt-3 mb-4'>Upload new images to the gallery</p>
-						<Link to='/admin/upload-images'>
-							<button className='bg-gray-800 px-4 py-2 rounded-md text-white'>
-								Upload images
-							</button>
-						</Link>
-					</div>
+
+					<div className='flex gap-8 mt-12 justify-end flex-col'>
+						<div className='border px-4 pt-2 pb-4 rounded w-72 backdrop-blur-lg bg-primary shadow-sm'>
+							<p className='text-xl font-lg font-bold text-gray-700'>Image Category</p>
+							<p className='text-text mt-3 mb-4'>Create a new category for images</p>
+							<Link to='/admin/add-category'>
+								<button className='bg-gray-800 px-4 py-2 rounded-md text-white'>
+									Add Category
+								</button>
+							</Link>
+						</div>
+						<div className='border px-4 pt-2 pb-4 rounded w-72 backdrop-blur-lg bg-primary shadow-sm'>
+							<p className='text-xl font-lg font-bold text-gray-700'>Upload Images</p>
+							<p className='text-text mt-3 mb-4'>Upload new images to the gallery</p>
+							<Link to='/admin/upload-images'>
+								<button className='bg-gray-800 px-4 py-2 rounded-md text-white'>
+									Upload images
+								</button>
+							</Link>
+						</div>
+					</div >
 				</div >
-			</div >
+			</div>
 
 		</>
 	)
